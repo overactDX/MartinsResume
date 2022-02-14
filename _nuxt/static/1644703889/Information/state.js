d40fd39215794d00d7c63f23161aa3fedc713188
overactDX <overactDX@users.noreply.github.com>
2022-02-14 15:48:05 +0000

    deploy: 2d348c627320cfb2ee438d590997e1253630cc04

--- _nuxt/static/1644703889/Information/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FMartinsResume\u002F_nuxt\u002Fstatic\u002F1644703889",layout:"default",error:a,serverRendered:true,routePath:"\u002FInformation",config:{_app:{basePath:"\u002FMartinsResume\u002F",assetsPath:"\u002FMartinsResume\u002F_nuxt\u002F",cdnURL:a}}}}(null));
